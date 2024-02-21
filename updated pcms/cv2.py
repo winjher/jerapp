632104e76d3f783782a57339248877ce0b464b02
winjher <138558440+winjher@users.noreply.github.com>
2024-02-21 09:53:23 +0800

    Add files via upload

---
+++ updated pcms/cv2.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+
+
+# Read the image
+img = cv2.imread('image.jpg')
+
+# Display the image
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+import cv2 as cv
+img = cv.imread("Predators/Lizards.jpg")
+
+cv.imshow('Lizards', img)
+def rescaleFrame (frame, scale = 0.75):
+      width = int(frame.shape[1] * scale)
+      height = int(frame.shape[0] * scale)
+      dimensions = (width,height)
+
+      return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+
+cv.waitkey(0)
+cv2.destroyAllWindows()
