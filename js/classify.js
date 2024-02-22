0004a9d544e6401f571facf0ffd74755c029a74f
winjher <138558440+winjher@users.noreply.github.com>
2024-02-22 09:43:02 +0800

    Add files via upload

--- js/classify.js
+++ js/classify.js
@@ -1,62 +1,26 @@
 
-let adult = [];
-let pupae = [];
-let larvae = [];
-let eggs = [];
-let pupaDefects = [];
-let pupaHealthy = [];
-let larvalDiseases = [];
-let larvaeHealthy = [];
+// Create a variable containing the result container
+const element = document.getElementById("imageResult");
 
-function preload(){
-    for (let i=0;i<100;i++){
-        let index = nf(i + 1,4,0);
-        adult[i] = loadImage(`data/adult${index}.jpg`);
-        pupae[i] = loadImage(`data/pupae${index}.jpg`);
-        larvae[i] = loadImage(`data/larvae${index}.jpg`);
-        eggs[i] = loadImage(`data/eggs${index}.jpg`);
-        pupaDefects[i] = loadImage(`data/pupaDefects${index}.jpg`);
-        pupaHealthy[i] = loadImage(`data/pupaHealthy${index}.jpg`);
-        larvalDiseases[i] = loadImage(`data/larvalDiseases${index}.jpg`);
-        larvaeHealthy[i] = loadImage(`data/larvaeHealthy${index}.jpg`); 
+function startImageScan() {
+    // Create a variable to initialize the ml5.js image classifier with MobileNet
+    const classifier = ml5.imageClassifier('MobileNet');
+
+    // Scan the uploaded image
+    classifier.classify(document.getElementById("uploadedImage"), imageScanResult)
+    element.innerHTML = "Loading...";
+}
+
+// Check for errors and display the results if there aren't any
+function imageScanResult(error, results) {
+    if (error) {
+        element.innerHTML = error;
+    } else {
+        let num = results[0].confidence * 100;
+        element.innerHTML = results[0].label + "   Confidence: " + num.toFixed(0) + " % ";
     }
 }
 
-let shapeClassifier;
-
-function setup(){
-    createCanvas(400, 400);
-    background(0);
-    image(squares[0],0,0,width, height);
-
-    let options = {
-        inputs: [64,64,4],
-        task: 'imageClassification',
-        debug: true,
-    };
-
-
-    shapeClassifier = ml5.neuralNetwork(options);
-    //edit this
-    for (let i=0; i < circles.length; i++){
-        // let input = { image: circles[i]};
-        // let target = {label: "circle"};
-        // shapeClassifier.addData(input, target);
-        // let input = ;
-        // let target = ;
-        shapeClassifier.addData({ image: circles[i]}, {label: "circle"});
-        shapeClassifier.addData({ image: squares[i]}, {label: "square"});
-        shapeClassifier.addData({ image: triangles[i]}, {label: "triangle"});
-
-    }
-    shapeClassifier.normalizeData();
-    shapeClassifier.train({epochs: 50},finishedTraining)
-
+function myReset() {
+document.getElementById("myForm").reset();
 }
-
-function finishedTraining(){
-    console.log("Finish training")
-    shapeClassifier.save("model.json")
-}
-
-
