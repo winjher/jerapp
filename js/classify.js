7c13946e55dedddfe1e22345330f15b964b8cd42
winjher <138558440+winjher@users.noreply.github.com>
2024-08-07 17:36:10 +0800

    update

--- js/classify.js
+++ js/classify.js
@@ -24,3 +24,58 @@ function imageScanResult(error, results) {
 function myReset() {
 document.getElementById("myForm").reset();
 }
+
+
+
+
+const classifier = knnClassifier.create()
+const webcamElement = document.getElementById("webcam")
+
+let net
+
+async function app() {
+  console.log("Loading mobilnet...")
+
+  net = await mobilenet.load()
+
+  console.log("Loaded model")
+
+  const webcam = await tf.data.webcam(webcamElement)
+
+  const addExample = async (classId) => {
+    const img = await webcam.capture()
+
+    const activation = net.infer(img, true)
+
+    classifier.addExample(activation, classId)
+
+    img.dispose()
+  }
+
+  document.getElementById("butterfly").addEventListener("click", () => addExample(0))
+  document.getElementById("pupae").addEventListener("click", () => addExample(1))
+  document.getElementById("larvae").addEventListener("click", () => addExample(2))
+  document.getElementById("eggs").addEventListener("click", () => addExample(2))
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture()
+
+      const activation = net.infer(img, "conv_preds")
+
+      const result = await classifier.predictClass(activation)
+
+      const classes = ["Butterfly", "Pupae", "Larvae", "Eggs"]
+
+      document.getElementById("console").innerText = `
+                prediction: ${classes[result.label]}\n
+                probabilty: ${result.confidences[result.label]}
+            `
+
+      img.dispose()
+    }
+
+    await tf.nextFrame()
+  }
+}
+
+app()
