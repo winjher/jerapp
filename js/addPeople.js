84563ca7c70788d58aefabcaeb293a847c871fd7
winjher <138558440+winjher@users.noreply.github.com>
2024-09-23 15:57:02 +0800

    update

---
+++ js/addPeople.js
@@ -0,0 +1,99 @@
+
+//adding  labels
+const classifier = knnClassifier.create()
+const webcamElement = document.getElementById("webcam")
+
+let net
+
+async function app() {
+  console.log("Loading mobilnet...")
+
+  net = await mobilenet.load()
+
+  console.log("Loaded model")
+
+  const webcam = await tf.data.webcam(webcamElement)
+
+  const addExample = async (classId) => {
+    const img = await webcam.capture()
+
+    const activation = net.infer(img, true)
+
+    classifier.addExample(activation, classId)
+
+    img.dispose()
+  }
+
+  //types of larval diseaasee
+  document.getElementById("sarah").addEventListener("click", () => addExample(1))
+  document.getElementById("vanessa").addEventListener("click", () => addExample(2))
+  document.getElementById("don").addEventListener("click", () => addExample(3))
+  document.getElementById("ira").addEventListener("click", () => addExample(4))
+  document.getElementById("jerwin").addEventListener("click", () => addExample(5))
+  document.getElementById("notpeople").addEventListener("click", () => addExample(6))
+
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture()
+
+      const activation = net.infer(img, "conv_preds")
+
+      const result = await classifier.predictClass(activation)
+
+      const classes = ["Sarah", "Vanessa", "Don", "Ira","Jerwin"]
+  
+      document.getElementById("console").innerText = `
+                Prediction: ${classes[result.label]}\n
+                Probabilty: ${result.confidences[result.label]}
+            `
+
+      img.dispose()
+    }
+
+    await tf.nextFrame()
+  }
+}
+
+// camera stream video element
+let on_stream_video = document.querySelector('#camera-stream');
+// flip button element
+let flipBtn = document.querySelector('#flip-btn');
+
+// default user media options
+let constraints = { audio: false, video: true }
+let shouldFaceUser = true;
+
+// check whether we can use facingMode
+let supports = navigator.mediaDevices.getSupportedConstraints();
+if( supports['facingMode'] === true ) {
+  flipBtn.disabled = false;
+}
+
+let stream = null;
+
+function capture() {
+  constraints.video = {
+      width: {
+      min: 192,
+      ideal: 192,
+      max: 192,
+    },
+    height: {
+      min: 192,
+      ideal: 192,
+      max: 192
+    },
+    facingMode: shouldFaceUser ? 'user' : 'environment'
+  }
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(mediaStream) {
+      stream  = mediaStream;
+      on_stream_video.srcObject = stream;
+      on_stream_video.play();
+    })
+    .catch(function(err) {
+      console.log(err)
+    });
+}
+app()
