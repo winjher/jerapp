9d771c0c17456e0dbeadf2afd93136504af26e5b
winjher <138558440+winjher@users.noreply.github.com>
2024-11-18 10:47:01 +0800

    classification.html

--- js/labelInfo.js
+++ js/labelInfo.js
@@ -1,19 +1,29 @@
 function processLabel() {
-      const predInfo = [
-            { commonName: 'Butterfly-Clippers', scientificName: 'Parthenos sylvia', discoveredBy: 'Cramer', year: '1775', family: 'Nymphalidae', larvalHostPlant: 'Adenia', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Common Jay', scientificName: 'Graphium doson', discoveredBy: 'C. & R. Felder', year: '1864', family: 'Papilionidae', larvalHostPlant: 'Annona',lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Plain Tiger', scientificName: 'Danaus chrysippus', discoveredBy: 'Linnaeus', year: '1758', family: 'Nymphalidae Danainae Danaini', larvalHostPlant: 'Cornflower', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Common Mormon', scientificName: 'Papilio polytes', discoveredBy: 'Eschscholtz', year: '1821', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Calamondin,Citrus Lemoncito', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Tailed Jay', scientificName: '𝘎𝘳𝘢𝘱𝘩𝘪𝘶𝘮 𝘢𝘨𝘢𝘮𝘦𝘮𝘯𝘰𝘯', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae Papilioninae Leptocircini', larvalHostPlant: 'Soursop,SUgar Apple, Amuyon', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Great Eggfly', scientificName: 'Hypolimnas bolina philippensis', discoveredBy: 'Butler', year: '1874', family: 'Nymphalidae Nymphalinae Nymphaliini', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Common Lime', scientificName: 'Papilio demoleus', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Calamondin,Citrus Lemoncito', lepidoptera:'Butterfly' },
-            { commonName: 'Butterfly-Three-Spot Grass Yellow', scientificName: 'Eurema blanda visellia', discoveredBy: 'Fruhstorfer', year: '1910', family: 'Pieridae; Coliadinae', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera:'Butterfly' },
-            { commonName: 'Skipper-Grass Skipper', scientificName: 'Prusiana prusias matinus', discoveredBy: 'Fruhstorfer', year: '1911', family: 'Hesperiidae; Hesperiinae', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera:'Skipper' },
-            { commonName: 'Butterfly-Helenus Hystaspe', scientificName: 'Menelaides helenus hystaspes', discoveredBy: 'C.& R. Felder', year: '1862', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Calamondin,Citrus Lemoncito', lepidoptera:'Butterfly' },
-            { commonName: 'Moth-Atlas', scientificName: 'Attacus Atlas', discoveredBy: 'Linnaeus', year: '1767', family: 'Saturniinae', larvalHostPlant: 'Sugar Apple, Soursop', lepidoptera:'Moth'},
-
-            // Add more species here...
+    const predInfo = [
+        { commonName: 'Butterfly-Batwing', scientificName: 'Atrophaneura semperi', discoveredBy: 'Cramer', year: '1775', family: 'Nymphalidae', larvalHostPlant: 'Dutchman pipe, Indian birtworth', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Clippers', scientificName: 'Parthenos sylvia', discoveredBy: 'Cramer', year: '1775', family: 'Nymphalidae', larvalHostPlant: 'Wild cucumber', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Common Jay', scientificName: 'Graphium doson', discoveredBy: 'C. & R. Felder', year: '1864', family: 'Papilionidae', larvalHostPlant: 'Avocado tree, Soursop, Sugar apple, Amuyon, Indian tree', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Common Lime', scientificName: 'Papilio demoleus', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Limeberry, Calamondin,Pomelo,Sweet orange, Calamansi Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Common Mime', scientificName: 'Chilasa clytia', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Clover cinnamon, Wild cinnamon', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Common Mormon', scientificName: 'Papilio polytes', discoveredBy: 'Eschscholtz', year: '1821', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Calamondin,Pomelo, Calamansi, Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Emerald SwallowTail', scientificName: 'Papilio palinurus', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae Papilionidae Lepto Circini', larvalHostPlant: 'Curry leaf, Pink Lime Berry Tree', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Golden Birdwing', scientificName: 'Troides rhadamantus', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae Papilionidae Lepto Circini', larvalHostPlant: 'Dutchman pipe, Indian Birthwort', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Gray Glassy Tiger', scientificName: 'Parantica aglea', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae Papilionidae Lepto Circini', larvalHostPlant: 'Soursop,SUgar Apple, Amuyon', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Great Eggfly', scientificName: 'Hypolimnas bolina philippensis', discoveredBy: 'Butler', year: '1874', family: 'Nymphalidae Nymphalinae Nymphaliini', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Great Yellow Mormon', scientificName: 'Papilio lowi', discoveredBy: 'Butler', year: '1874', family: 'Nymphalidae Nymphalinae Nymphaliini', larvalHostPlant: 'Limeberry, Calamondin,Pomelo,Sweet orange, Calamansi Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Paper Kite', scientificName: 'Idea Leucono', discoveredBy: 'Butler', year: '1874', family: 'Nymphalidae Nymphalinae Nymphaliini', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Pink Rose', scientificName: 'Pachliopta kutzebuea', discoveredBy: 'Linnaeus', year: '1758', family: 'Nymphalidae Danainae Danaini', larvalHostPlant: 'Cornflower', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Plain Tiger', scientificName: 'Danaus chrysippus', discoveredBy: 'Linnaeus', year: '1758', family: 'Nymphalidae Danainae Danaini', larvalHostPlant: 'Cornflower', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Red Lacewing', scientificName: 'Cethosia biblis', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Wild Bush Passion Fruit,Citrus Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Scarlet Mormon', scientificName: 'Papilio romansovia', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Limeberry, Calamondin,Pomelo,Sweet orange, Calamansi Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Tailed Jay', scientificName: '𝘎𝘳𝘢𝘱𝘩𝘪𝘶𝘮 𝘢𝘨𝘢𝘮𝘦𝘮𝘯𝘰𝘯', discoveredBy: 'Linnaeus', year: '1758', family: 'Papilionidae Papilionidae Lepto Circini', larvalHostPlant: 'Soursop,SUgar Apple, Amuyon', lepidoptera: 'Butterfly' },
+        { commonName: 'Butterfly-Three-Spot Grass Yellow', scientificName: 'Eurema blanda visellia', discoveredBy: 'Fruhstorfer', year: '1910', family: 'Pieridae; Coliadinae', larvalHostPlant: 'Wild Peanut', lepidoptera: 'Butterfly' },
+        { commonName: 'Skipper-Grass Skipper', scientificName: 'Prusiana prusias martinus', discoveredBy: 'Fruhstorfer', year: '1911', family: 'Hesperiidae; Hesperiinae', larvalHostPlant: 'Sweet Potato, Water Spinach', lepidoptera: 'Skipper' },
+        { commonName: 'Butterfly-Helenus Hystaspe', scientificName: 'Menelaides helenus hystaspes', discoveredBy: 'C.& R. Felder', year: '1862', family: 'Papilionidae; Papilioninae; Papilionini', larvalHostPlant: 'Calamondin,Citrus Lemoncito', lepidoptera: 'Butterfly' },
+        { commonName: 'Moth-Atlas', scientificName: 'Attacus Atlas', discoveredBy: 'Linnaeus', year: '1767', family: 'Saturniidae', larvalHostPlant: 'Sugar Apple, Soursop, Gimelina', lepidoptera: 'Moth' }
+// Add more species here...
         ];
+
       const imgInput = document.getElementById('imageUpload');
       const outputDiv = document.getElementById('output');
       outputDiv.innerHTML = '';
@@ -47,3 +57,71 @@ function processLabel() {
   }
 
   document.getElementById('imageUpload').addEventListener('change', processLabel);
+
+  function checkButterfly(species) {
+    switch (species) {
+      case 'Butterfly-Batwing':
+        // Code for Butterfly-Batwing
+        const firstElement = predInfo[0];
+        console.log(firstElement);
+
+        break;
+      case 'Butterfly-Clippers':
+        // Code for Butterfly-Clippers
+        break;
+      case 'Butterfly-Common Jay':
+        // Code for Butterfly-Common Jay
+        break;
+      case 'Butterfly-Common Lime':
+        // Code for Butterfly-Common Lime
+        break;
+      case 'Butterfly-Common Mime':
+        // Code for Butterfly-Common Mime
+        break;
+      case 'Butterfly-Common Mormon':
+        // Code for Butterfly-Common Mormon
+        break;
+      case 'Butterfly-Emerald Swallowtail':
+        // Code for Butterfly-Emerald Swallowtail
+        break;
+      case 'Butterfly-Golden Birdwing':
+        // Code for Butterfly-Golden Birdwing
+        break;
+      case 'Butterfly-Gray Glassy Tiger':
+        // Code for Butterfly-Gray Glassy Tiger
+        break;
+      case 'Butterfly-Great Eggfly':
+        // Code for Butterfly-Great Eggfly
+        break;
+      case 'Butterfly-Great Yellow Mormon':
+        // Code for Butterfly-Great Yellow Mormon
+        break;
+      case 'Butterfly-Paper Kite':
+        // Code for Butterfly-Paper Kite
+        break;
+      case 'Butterfly-Plain Tiger':
+        // Code for Butterfly-Plain Tiger
+        break;
+      case 'Butterfly-Red Lacewing':
+        // Code for Butterfly-Red Lacewing
+        break;
+      case 'Butterfly-Scarlet Mormon':
+        // Code for Butterfly-Scarlet Mormon
+        break;
+      case 'Butterfly-Pink Rose':
+        // Code for Butterfly-Pink Rose
+        break;
+      case 'Butterfly-Tailed Jay':
+        // Code for Butterfly-Tailed Jay
+        break;
+      case 'Moth-Giant Silk':
+        // Code for Moth-Giant Silk
+        break;
+      case 'Moth-Atlas':
+        // Code for Moth-Atlas
+        break;
+      default:
+        // Code for unknown species
+    }
+  }
+  
--- js/upload.js
+++ js/upload.js
@@ -162,7 +162,7 @@
             const larvae=['Larvae Common Jay', 'Larvae Instar-Common Lime', 'Larvae Instar-Golden Birdwing', 'Larvae-Atlas', 'Larvae-Clippers', 'Larvae-Common Jay', 'Larvae-Common Lime', 'Larvae-Eggs-Golden Birdwing', 'Larvae-Giant Silk Moth', 'Larvae-Golden Birdwing', 'Larvae-Paper Kite', 'Larvae-Pink Rose', 'Larvae-Plain Tiger', 'Larvae-Red Lacewing', 'Larvae-Tailed Jay'];
             const eggs =['Eggs-Common Jay', 'Eggs-Common Lime', 'Eggs-Golden Birdwing', 'Eggs-Pink Rose', 'Eggs-Plain Tiger', 'Eggs-Tailed Jay', 'Egs-Red Lacewing', 'Healthy Larvae-Golden Birdwing', 'Healthy Pupae-Common Lime', 'Healthy Pupae-Golden Birdwing', 'Moth-Atlas', 'Moth-Giant Silk', 'Old Pupae-Plain Tiger', 'Ovipositing-Tailed Jay', 'Predators Ants-Golden Birdwing', 'Prepupae Larvae-Golden Birdwing', 'Pupae-Common Jay', 'Pupae-Common Lime', 'Pupae-Golden Birdwing', 'Pupae-Golding Brdwing', 'Pupae-Paper Kite', 'Pupae-Pink Rose', 'Pupae-Plain Tiger', 'Pupae-Tailed Jay', 'Skipper-Club Silverline'];
             const diseases=['Disease Larvae Atlas','Disease Larvae Golden Birdwing','Disease Larvae Plain Tiger'];
-            const diseasesType=['Disease Type','Disease Type Tachinids Fly', '']
+            const diseasesType=['Disease Type','Disease Type Tachinid Fly', '']
             const predLep = ['Butterfly','Moth','Skipper'];
             const fname=['ryan','christian','malik','james','she', 'Jao','ming','allan'];
             const spotlocation= ['Manila Philippines','Bago Negros Occ.','Baguio City'];
